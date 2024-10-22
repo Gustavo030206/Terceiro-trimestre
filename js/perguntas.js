1973a9ca39cb92ac303a4609d3b2a35e6af90dea
Gustavo030206 <148063941+Gustavo030206@users.noreply.github.com>
2024-10-21 23:11:23 -0300

    Create perguntas.js

---
+++ js/perguntas.js
@@ -0,0 +1,120 @@
+export const perguntas = [
+    {
+        enunciado: "Você assiste gameplay de algum youtuber ?",
+        alternativas: [
+            {
+                texto: "Sim",
+                afirmacao: [
+                    "Ao assistir, ele está sendo um inútil"
+                    ],
+                proxima: 1,
+            },
+            {
+                texto: "Não",
+                afirmacao: [
+                    "Ao não assistir, ele está sendo produtivo em porcaria nenhuma"
+                    ],
+                proxima: 1, 
+            },
+        ]
+    },
+    
+    {
+        enunciado: "Você gosta de jogar jogos de terror ?",
+        alternativas: [
+            {
+                texto:"Sim",
+                afirmacao: [
+                    " e que ao gostar de jogo de terror ele não tem medo. "
+                    ],
+                proxima: 2, 
+            },
+            {
+                texto: "Não, tenho medo",
+                afirmacao: [
+                    " e que ao gostar de jogo de terror ele tem medo. "
+                    ],
+                proxima: 2, 
+            },
+        ]
+    },
+    
+    {
+        enunciado: "Você prefere que tipo de plataforma ?",
+        alternativas: [
+            {
+                texto:"Consoles",
+                afirmacao: [
+                    "Quando ele escolhe console ele mostra o quão burro ele é,"
+                    ],
+                proxima: 3, 
+            },
+            {
+                texto:"Computador",
+                afirmacao: [
+                    "Quando ele escolhe PC ele mostra o quão sábio ele é, "
+                ],
+                proxima: 3, 
+            },
+        ]
+    },
+    
+    {
+        enunciado: "Você acha justo o preço cobrados nos jogos ?",
+        alternativas: [
+            {
+                texto: "Sim",
+                afirmacao: [
+                    "e que quando ele acha o preço dos jogos justo, ele mostra o quão burro financeiramente é"
+                    ],
+                proxima: 4, 
+            },
+            {
+                texto: "Não, eu sou inteligente",
+                afirmacao: [
+                    "e que quando ele acha o preço dos jogos justo, ele mostra o quão burro financeiramente é"
+                ],
+                proxima: 4, 
+            },     
+        ]
+    },
+    
+    {
+        enunciado: "Você é a favor da pirataria ?",
+        alternativas: [
+            {
+                texto: "Sim",
+                afirmacao: [
+                    ". O cara lembrou que mora no Brasil "
+                    ],
+                proxima: 5, 
+            },
+            {
+                texto: "Não, eu sou um bosta",
+                afirmacao: [
+                    ". O cara esqueceu que mora no Brasil "
+                    ],
+                proxima: 5, 
+            },
+        ]
+    },
+    
+    {
+        enunciado: "Você gostaria de trabalhar com a programação de jogos ?",
+        alternativas: [
+            {
+                texto: "Sim",
+                afirmacao: [
+                    " mal sabia ele(a) que irá passar fome"
+                    ]
+            },
+            {
+                texto: "Não",
+                afirmacao: [
+                    " mal sabia ele que escapou da fome, amém !"
+                    ] 
+            },   
+        ]
+    },
+];
+
